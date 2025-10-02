7189b4dcce4fd0a3faab2814174271db686f16ff
infomotin <motin.mmk.jr@gmail.com>
2025-10-02 14:39:44 +0600

    Update Jobs Batch With Conditions

--- queue-app/routes/web.php
+++ queue-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmailController;
 
@@ -25,41 +26,70 @@ Route::get('/', function () {
     // \App\Jobs\SendWelcomeEmail::dispatch();
     // \App\Jobs\ProcessPayment::dispatch()->onQueue('payments');
     // \App\Jobs\SeedEmail::dispatch();
-    $batch = [
-        // new \App\Jobs\SendWelcomeEmail('email1'),
-        // new \App\Jobs\SendWelcomeEmail('email2'),
-        // new \App\Jobs\SendWelcomeEmail('email3'),
+    // $batch = [
+    //     // new \App\Jobs\SendWelcomeEmail('email1'),
+    //     // new \App\Jobs\SendWelcomeEmail('email2'),
+    //     // new \App\Jobs\SendWelcomeEmail('email3'),
 
+    //     // new \App\Jobs\SeedEmail(),
+    //     // new \App\Jobs\ProcessPayment(),
+    //     [
+    //         new \App\Jobs\ProcessPayment('payment1'),
+    //         new \App\Jobs\SeedEmail('payment2'),
+    //         new \App\Jobs\SendWelcomeEmail('payment3'),
+    //     ],
+    //     [
+    //         new \App\Jobs\ProcessPayment('payment1'),
+    //         new \App\Jobs\SeedEmail('payment2'),
+    //         new \App\Jobs\SendWelcomeEmail('payment3'),
+    //     ]
+
+    // ];
+    // \Illuminate\Support\Facades\Bus::batch($batch)
+    //     ->allowFailures(true) // allow failures for individual jobs
+    //     // ->then(function (\Illuminate\Bus\Batch $batch) {
+    //     //     // All jobs completed successfully...
+    //     //     info('All jobs completed successfully...');
+    //     // })
+    //     // ->catch(function (\Illuminate\Bus\Batch $batch, \Throwable $e) {
+    //     //     // First batch job failure detected...
+    //     //     info('First batch job failure detected...');
+    //     // })
+    //     // ->onQueue('processing') // specify the queue for the batch
+    //     // ->onConnection('database') // specify the connection for the batch
+    //     // ->finally(function (\Illuminate\Bus\Batch $batch) {
+    //     //     // The batch has finished executing...
+    //     //     info('The batch has finished executing...');
+    //     // })
+    //     ->dispatch();
+
+    Bus::chain([
+        new \App\Jobs\ProcessPayment(),
         // new \App\Jobs\SeedEmail(),
-        // new \App\Jobs\ProcessPayment(),
-        [
-            new \App\Jobs\ProcessPayment('payment1'),
-            new \App\Jobs\SeedEmail('payment2'),
-            new \App\Jobs\SendWelcomeEmail('payment3'),
-        ],
-        [
-            new \App\Jobs\ProcessPayment('payment1'),
-            new \App\Jobs\SeedEmail('payment2'),
-            new \App\Jobs\SendWelcomeEmail('payment3'),
-        ]
+        // new \App\Jobs\SendWelcomeEmail(),
+        function () {
+            Bus::batch([
+                new \App\Jobs\SeedEmail(),
+                new \App\Jobs\SendWelcomeEmail(),
+            ])->then(function (\Illuminate\Bus\Batch $batch) {
+                // All jobs completed successfully...
+                info('All jobs completed successfully...');
+            })->catch(function (\Illuminate\Bus\Batch $batch, \Throwable $e) {
+                // First batch job failure detected...
+                info('First batch job failure detected...');
+            })->finally(function (\Illuminate\Bus\Batch $batch) {
+                // The batch has finished executing...
+                info('The batch has finished executing...');
+            })->allowFailures(true)->dispatch();
+        },
+        function () {
+            Bus::batch([
+                new \App\Jobs\ProcessPayment(),
+                new \App\Jobs\SendWelcomeEmail(),
+            ])->dispatch();
+        }
+    ]);
 
-    ];
-    \Illuminate\Support\Facades\Bus::batch($batch)
-        ->allowFailures(true) // allow failures for individual jobs
-        // ->then(function (\Illuminate\Bus\Batch $batch) {
-        //     // All jobs completed successfully...
-        //     info('All jobs completed successfully...');
-        // })
-        // ->catch(function (\Illuminate\Bus\Batch $batch, \Throwable $e) {
-        //     // First batch job failure detected...
-        //     info('First batch job failure detected...');
-        // })
-        // ->onQueue('processing') // specify the queue for the batch
-        // ->onConnection('database') // specify the connection for the batch
-        // ->finally(function (\Illuminate\Bus\Batch $batch) {
-        //     // The batch has finished executing...
-        //     info('The batch has finished executing...');
-        // })
-        ->dispatch();
+
     return view('welcome');
 });
